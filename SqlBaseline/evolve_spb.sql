84a4db9b262e0020c74bf03dab18f7ee8f7735d9
Chakradhar Jeereddy <79322615+Chakradhar-Jeereddy@users.noreply.github.com>
2023-05-21 15:11:37 -0400

    evolve baseline using sqlhandle

--- SqlBaseline/evolve_spb.sql
+++ SqlBaseline/evolve_spb.sql
@@ -1,16 +1,4 @@
-REM $Header: 215187.1 evolve_spb.sql 11.4.5.8 2013/05/10 carlos.sierra $
-
-ACC sql_text_piece PROMPT 'Enter SQL Text piece: '
-
 SET PAGES 200 LONG 80000 ECHO ON;
-
-COL sql_text PRI;
-
-SELECT sql_handle, plan_name, sql_text /* exclude_me */
-  FROM dba_sql_plan_baselines
- WHERE sql_text LIKE '%&&sql_text_piece.%'
-   AND sql_text NOT LIKE '%/* exclude_me */%';
-
 ACC sql_handle PROMPT 'Enter SQL Handle: ';
 
 SELECT sql_handle, sql_text /* exclude_me */
@@ -33,10 +21,6 @@ SELECT plan_name, enabled, accepted, fixed, reproduced, created /* exclude_me */
  ORDER BY
        created;
 
-ACC verify_par PROMPT 'Enter optional verify parameter (YES/NO): ';
-
-ACC commit_par PROMPT 'Enter optional commit parameter (NO/YES): '
-
 VAR x CLOB;
 
 BEGIN
@@ -58,4 +42,4 @@ SPO OFF;
 
 SET PAGES 14 LONG 80 LIN 80 TRIMS OFF ECHO OFF FEED 6 HEA ON;
 
-UNDEF sql_text_piece sql_handle plan_name verify_par commit_par
+UNDEF  sql_handle plan_name verify_par commit_par
