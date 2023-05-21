ec9f1ea708a44b7011ee689f2f1e189379fe5a4f
Chakradhar Jeereddy <79322615+Chakradhar-Jeereddy@users.noreply.github.com>
2023-05-21 14:47:28 -0400

    Display plan from baseline using sql handle

--- SqlBaseline/display_spb.sql
+++ SqlBaseline/display_spb.sql
@@ -1,16 +1,4 @@
-REM $Header: 215187.1 display_spb.sql 11.4.5.8 2013/05/10 carlos.sierra $
-
-ACC sql_text_piece PROMPT 'Enter SQL Text piece: '
-
 SET PAGES 200 LONG 80000 ECHO ON;
-
-COL sql_text PRI;
-
-SELECT sql_handle, plan_name, sql_text /* exclude_me */
-  FROM dba_sql_plan_baselines
- WHERE sql_text LIKE '%&&sql_text_piece.%'
-   AND sql_text NOT LIKE '%/* exclude_me */%';
-
 ACC sql_handle PROMPT 'Enter SQL Handle: ';
 
 SELECT sql_handle, sql_text /* exclude_me */
