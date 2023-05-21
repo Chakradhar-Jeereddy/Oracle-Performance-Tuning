537b02978530247484801246bb70ad68655238af
Chakradhar Jeereddy <79322615+Chakradhar-Jeereddy@users.noreply.github.com>
2023-05-21 14:33:42 -0400

    Display tuning set of SQLID

--- SqlBaseline/display_sts.sql
+++ SqlBaseline/display_sts.sql
@@ -1,18 +1,4 @@
-REM $Header: 215187.1 display_sts.sql 11.4.5.8 2013/05/10 carlos.sierra $
-
-PAU Requires Oracle Tuning Pack license. Hit "Enter" to proceed
-
-ACC sql_text_piece PROMPT 'Enter SQL Text piece: '
-
 SET PAGES 200 LONG 80000 ECHO ON;
-
-COL sql_text PRI;
-
-SELECT sql_id, sql_text /* exclude_me */
-  FROM dba_sqlset_statements
- WHERE sql_text LIKE '%&&sql_text_piece.%'
-   AND sql_text NOT LIKE '%/* exclude_me */%';
-
 ACC sql_id PROMPT 'Enter SQL_ID: ';
 
 SELECT sqlset_name, sqlset_owner /* exclude_me */
