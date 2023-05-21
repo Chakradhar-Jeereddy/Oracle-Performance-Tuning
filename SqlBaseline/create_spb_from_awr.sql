ad9607910fc170e41d2b772473ddd2cdd8ce5646
Chakradhar Jeereddy <79322615+Chakradhar-Jeereddy@users.noreply.github.com>
2023-05-21 14:10:36 -0400

    Takes plan from awr creates STS and loads into dictionary

--- SqlBaseline/create_spb_from_awr.sql
+++ SqlBaseline/create_spb_from_awr.sql
@@ -1,34 +1,21 @@
-REM $Header: 215187.1 create_spb_from_awr.sql 11.4.5.8 2013/05/10 carlos.sierra $
-
-PAU Requires Oracle Tuning Pack license. Hit "Enter" to proceed
-
-ACC sql_text_piece PROMPT 'Enter SQL Text piece: '
-
 SET PAGES 200 LONG 80000 ECHO ON;
-
-COL sql_text PRI;
-
-SELECT dbid, sql_id, sql_text /* exclude_me */
-  FROM dba_hist_sqltext
- WHERE sql_text LIKE '%&&sql_text_piece.%'
-   AND sql_text NOT LIKE '%/* exclude_me */%';
-
-ACC dbid NUM PROMPT 'Enter DBID: ';
-
 ACC sql_id PROMPT 'Enter SQL_ID: ';
 
-SELECT p.snap_id, s.startup_time, s.begin_interval_time, s.end_interval_time, /* exclude_me */
-       p.instance_number, p.plan_hash_value, p.executions_total, p.elapsed_time_total,
-       CASE WHEN p.executions_total > 0 THEN ROUND(p.elapsed_time_total/p.executions_total/1e6, 3) END avg_secs_per_exec
-  FROM dba_hist_sqlstat p,
-       dba_hist_snapshot s
- WHERE p.dbid = &&dbid
-   AND p.sql_id = '&&sql_id.'
-   AND s.snap_id = p.snap_id
-   AND s.dbid = p.dbid
-   AND s.instance_number = p.instance_number
- ORDER BY
-       p.snap_id, p.plan_hash_value;
+set lines 155
+col execs for 999,999,999
+col avg_etime for 999,999,999
+col avg_lreads for 999,999,999
+col begin_interval_time for a30
+col node for 99999
+break on plan_hash_value on startup_time skip 1
+select ss.snap_id,ss.instance_number node, begin_interval_time,sql_id,plan_hash_value,
+nvl(executions_delta,0) execs,(elapsed_time_delta/decode(nvl(executions_detla,0),0,1,executions_delta)/1000000 avg_etime,
+(buffer_gets_delta/decode(nvl(buffer_gets_delt,0),0,1,executions_delta) avg_lreads
+from dba_hist_sqlstat s,dba_hist_snapshot ss
+where sql_id = '&1'
+and ss.snap_id=s.snap_id
+and ss.instance_number=s.instance_number
+and executions_delta >0 order by 1,2,3;
 
 ACC plan_hash_value PROMPT 'Enter Plan Hash Value: ';
 
